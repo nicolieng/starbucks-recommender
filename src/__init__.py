68b12ecb48b7b3359d644689b6aa0250ba145a21
Nicolie Ng <135363095+nicolieng@users.noreply.github.com>
2025-05-09 13:08:49 -0400

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .recommendation import run_recommender_system2
+from .user_input import get_user_input
+from .feature_engineering import add_taste_profile
+from .data_cleaning import clean_data
