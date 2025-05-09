bd61043a0815d4f136e51bed81f8335b2e82f4f4
Nicolie Ng <135363095+nicolieng@users.noreply.github.com>
2025-05-09 13:08:21 -0400

    Delete data/src directory

--- data/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .recommendation import run_recommender_system2
-from .user_input import get_user_input
-from .feature_engineering import add_taste_profile
-from .data_cleaning import clean_data
